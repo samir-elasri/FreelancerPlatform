bb2bf3963849c70acf260598b28e7993143384f5
Manar-Bakhat <manarbakhatt@gmail.com>
2023-01-09 12:25:22 +0100

    Seconde Change  Manar

--- FreelancerPlatform/Controllers/HomeController.cs
+++ FreelancerPlatform/Controllers/HomeController.cs
@@ -22,6 +22,10 @@ namespace FreelancerPlatform.Controllers
         {
             return View();
         }
+        public IActionResult Project()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
