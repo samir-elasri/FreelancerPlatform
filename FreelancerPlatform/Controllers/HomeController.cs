ad15fd9312bb1e886260f4ad259aa36a6f459afd
Manar-Bakhat <manarbakhatt@gmail.com>
2023-01-11 14:14:34 +0100

    manar_update

--- FreelancerPlatform/Controllers/HomeController.cs
+++ FreelancerPlatform/Controllers/HomeController.cs
@@ -26,6 +26,14 @@ namespace FreelancerPlatform.Controllers
         {
             return View();
         }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
