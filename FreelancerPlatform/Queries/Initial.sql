73fe84f7ebb553a107b90d6e68852b0997f6d336
Samir El Asri <samirel733@gmail.com>
2023-01-14 17:58:34 +0100

    Added Comments Functionality

--- FreelancerPlatform/Queries/Initial.sql
+++ FreelancerPlatform/Queries/Initial.sql
@@ -9,3 +9,7 @@ insert into tasks values('do that', 'bla bla bla', 1);
 
 --use FreelancerPlatform;
 --insert into users values('Sam', 'sam@gmail.com', 'sam', 'freelancer');
+
+use FreelancerPlatform;
+insert into comments values('comment one', 6);
+insert into comments values('comment two', 6);
