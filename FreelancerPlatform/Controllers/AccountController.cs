f413191f878afd706e0774369c826e1748a4609c
Samir El Asri <samirel733@gmail.com>
2023-01-16 21:23:43 +0100

    Authentication using sessions

---
+++ FreelancerPlatform/Controllers/AccountController.cs
@@ -0,0 +1,45 @@
+﻿using FreelancerPlatform.Data;
+using FreelancerPlatform.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreelancerPlatform.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AccountController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(User user)
+        {
+            var userInDb = _context.Users.SingleOrDefault(u => u.Email == user.Email && u.Password == user.Password);
+
+            if (userInDb == null)
+            {
+                ModelState.AddModelError("Email", "Invalid Email");
+                ModelState.AddModelError("Password", "Invalid Password");
+                return View();
+            }
+
+            HttpContext.Session.SetString("UserName", userInDb.Name);
+            HttpContext.Session.SetString("UserEmail", userInDb.Email);
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
--- FreelancerPlatform/Program.cs
+++ FreelancerPlatform/Program.cs
@@ -11,6 +11,14 @@ builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlSer
     builder.Configuration.GetConnectionString("DefaultConnection")
 ));
 
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.Cookie.Name = ".AdventureWorks.Session";
+    options.IdleTimeout = TimeSpan.FromMinutes(10);
+    options.Cookie.IsEssential = true;
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -27,6 +35,9 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+
+app.UseSession();
+app.MapRazorPages();
 
 app.MapControllerRoute(
     name: "default",
